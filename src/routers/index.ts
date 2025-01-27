43937b132e0e3ced59131381aaa3b649d7ad4525
Agshin <znagshin@gmail.com>
2025-01-27 16:41:53 +0400

    third

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.router";
+import blogRouter from "./blogs.router";
 
 
 const router = Router()
 
 router.use("/auth" , authRouter)
+router.use("/blogs", blogRouter )
 
 export default router
